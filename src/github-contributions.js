9ef7690809751645f4845a8f3701d0a07b8b1f6d
Nick Portokallidis <portokallidis@gmail.com>
2016-07-09 20:04:39 +0000

    first commit from generator

---
+++ src/github-contributions.js
@@ -0,0 +1,62 @@
+'use strict';
+
+angular.module('np.github-contributions', []).directive('githubContributions', function($http) {
+  return {
+    restrict: 'E',
+    link: function(scope, ielem, attr) {
+
+      var options = attr.options || {};
+      var username = attr.username;
+      var titleCssClass = "github-contributions-title";
+      var graphCssClass = "github-contributions-graph";
+      if (!username) {
+        console.error("No username supplied");
+        return;
+      }
+      var proxy = options.proxy || function(url) {
+        return "https://urlreq.appspot.com/req?method=GET&url=" + url;
+      };
+      $http.get(proxy("https://github.com/" + username), {
+        cache: true
+      }).then(function(res) {
+
+        var graphtext = res.data.match(/<div class=\"boxed-group flush\">[\s\S]*(.*?)<\/div>/);
+        if (!graphtext) {
+          console.debug("No graph found, check github");
+          return;
+        }
+
+        var elem = document.createElement('div');
+        elem.innerHTML = graphtext;
+
+        if (!options.notitle) {
+          // contributions title sum number   
+          var title = elem.querySelector('h3');
+          title.className = titleCssClass;
+          title.innerHTML = title.textContent.trim();
+          ielem.append(title);
+        }
+
+        if (!options.nograph) {
+          // contributions graph - scalable!  
+          var graph = elem.querySelector('svg');
+          var gW = graph.getAttribute("width");
+          var gH = graph.getAttribute("height");
+          graph.setAttribute("height", "100%");
+          graph.setAttribute("width", "100%");
+          graph.setAttribute("height", "100%");
+          graph.setAttribute("viewBox", "0 0 " + gW + " " + gH);
+          graph.setAttribute("class", graphCssClass);
+          ielem.append(graph);
+        }
+
+      });
+
+
+
+
+    }
+  };
+
+
+});
---
+++ test/github-contributions.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe('github-contributions', function () {
+  var scope, $compile, $rootScope, element;
+
+  function createDirective(template) {
+    var elm;
+
+    elm = angular.element(template);
+    angular.element(document.body).prepend(elm);
+    $compile(elm)(scope);
+    scope.$digest();
+
+    return elm;
+  }
+
+  beforeEach(module('ngSanitize', 'np.github-contributions'));
+  beforeEach(inject(function(_$rootScope_, _$compile_) {
+    $rootScope = _$rootScope_;
+    scope = $rootScope.$new();
+    $compile = _$compile_;
+  }));
+
+  afterEach(function () {
+    if (element) element.remove();
+  });
+
+  describe('as an element', function(){ runTestsWithTemplate('<github-contributions></github-contributions>'); });
+
+  function runTestsWithTemplate(template) {
+    describe('when created', function () {
+
+      it('should initial the value to 0', function () {
+        element = createDirective(template);
+
+        expect(element.text()).toContain('0');
+      });
+    });
+  }
+
+});
