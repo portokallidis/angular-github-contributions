7f87204db188a50db96392c8ec1c3a57ba9c4de7
Nick Portokallidis <portokallidis@gmail.com>
2016-09-14 17:56:30 -0400

    update github graph selector

--- src/github-contributions.js
+++ src/github-contributions.js
@@ -7,7 +7,6 @@ angular.module('np.github-contributions', []).directive('githubContributions', f
 
       var options = attr.options || {};
       var username = attr.username;
-      var titleCssClass = "github-contributions-title";
       var graphCssClass = "github-contributions-graph";
       if (!username) {
         console.error("No username supplied");
@@ -19,36 +18,30 @@ angular.module('np.github-contributions', []).directive('githubContributions', f
       $http.get(proxy("https://github.com/" + username), {
         cache: true
       }).then(function(res) {
-
-        var graphtext = res.data.match(/<div class=\"boxed-group flush\">[\s\S]*(.*?)<\/div>/);
+        // remove text svg tags
+        var removeText=new RegExp(/<text[\s\S]*(.*?)<\/text>/,"g");
+        
+        // match graph
+        var matchGraph=new RegExp(/<div class=\"js-calendar-graph[\s\S]*(.*?)<\/div>/,"g");
+        
+        var graphtext = res.data.match(matchGraph);
+        
         if (!graphtext) {
           console.debug("No graph found, check github");
           return;
         }
-
         var elem = document.createElement('div');
-        elem.innerHTML = graphtext;
-
-        if (!options.notitle) {
-          // contributions title sum number   
-          var title = elem.querySelector('h3');
-          title.className = titleCssClass;
-          title.innerHTML = title.textContent.trim();
-          ielem.append(title);
-        }
-
-        if (!options.nograph) {
-          // contributions graph - scalable!  
-          var graph = elem.querySelector('svg');
-          var gW = graph.getAttribute("width");
-          var gH = graph.getAttribute("height");
-          graph.setAttribute("height", "100%");
-          graph.setAttribute("width", "100%");
-          graph.setAttribute("height", "100%");
-          graph.setAttribute("viewBox", "0 0 " + gW + " " + gH);
-          graph.setAttribute("class", graphCssClass);
-          ielem.append(graph);
-        }
+        elem.innerHTML = graphtext[0].toString().replace(removeText,"");
+        
+        // contributions graph - scalable!  
+        var graph = elem.querySelector('svg');
+        var gW = graph.getAttribute("width");
+        var gH = graph.getAttribute("height");
+        graph.setAttribute("width", "100%");
+        graph.setAttribute("height", "100%");
+        graph.setAttribute("viewBox", "14 8 " + gW + " " + gH);
+        graph.setAttribute("class", graphCssClass);
+        ielem.append(graph);
 
       });
 
